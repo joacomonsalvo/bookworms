d30b967956590f195a1c1f779f990abf8afbe270
joacomonsalvo <jmonsalvo.business@protonmail.com>
2025-07-14 20:23:51 -0300

    updated PORT

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-#!/bin/bash
-reflex run --env prod --loglevel info --backend-port $PORT
+echo "──────────────────────────── Initializing BookWorms ────────────────────────────"
+
+# Inicializa la web y luego ejecuta en el puerto de Render
+reflex init --force
+
+# Ejecuta Reflex en el puerto proporcionado por Render
+reflex run --backend-host 0.0.0.0 --backend-port $PORT
