41c8babfe97c8cb285053fa8f1661757621cefb7
joacomonsalvo <jmonsalvo.business@protonmail.com>
2025-07-14 20:57:29 -0300

    updated PORT

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-
 echo "──────────────────────────── Starting BookWorms ────────────────────────────"
 
-reflex run --frontend-port $PORT
+# Init si es necesario (opcional)
+reflex init || true
 
+# Build el frontend (si no está)
+reflex export --frontend-only
+
+# Ejecutar el backend con preview
+reflex run --frontend-only --backend-host 0.0.0.0 --backend-port 8000 --frontend-port 10000 --backend-only=false --loglevel debug --env prod
