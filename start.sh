bcdd2404822c1f202e3bb99c3800cdf7da15a747
joacomonsalvo <jmonsalvo.business@protonmail.com>
2025-07-14 21:29:38 -0300

    updated PORT

--- start.sh
+++ start.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # (solo la primera vez) para recrear el web build
-# Esto asegura que los archivos estáticos del frontend estén construidos.
 reflex init || true
 
 # Arranca Reflex en producción, poniendo FRONTEND y BACKEND en el mismo puerto
-# Usamos '0.0.0.0' para que escuche en todas las interfaces de red, no solo localhost.
-# La variable $PORT es proporcionada automáticamente por Render.
 reflex run \
   --env prod \
   --loglevel info \
-  --host 0.0.0.0 \
+  --backend-host 0.0.0.0 \
   --port $PORT
